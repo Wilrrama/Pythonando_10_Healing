82ae0a123145e5f61f3cad22fc4afbff9945f862
wilrrama <wilrrama@hotmail.com>
2024-04-22 18:29:47 -0300

    minhas consultas

--- medico/views.py
+++ medico/views.py
@@ -1,8 +1,9 @@
 from django.contrib import messages
 from django.contrib.messages import constants
 from django.shortcuts import redirect, render
+from paciente.models import Consulta
 from .models import DadosMedico, Especialidades, is_medico, DatasAbertas
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Create your views here.
 def cadastro_medico(request): 
@@ -76,5 +77,18 @@ def abrir_horario(request):
 
         messages.add_message(request, constants.SUCCESS, 'Horário cadastrado com sucesso.')
         return redirect('/medicos/abrir_horario')
+    
+def consultas_medico(request):
+    if not is_medico(request.user):
+        messages.add_message(request, constants.WARNING, 'Somente médicos podem acessar essa página.')
+        return redirect('/usuarios/sair')
+    
+    hoje = datetime.now().date()
+
+    consultas_hoje = Consulta.objects.filter(data_aberta__user=request.user).filter(data_aberta__data__gte=hoje).filter(data_aberta__data__lt=hoje + timedelta(days=1))
+    consultas_restantes = Consulta.objects.exclude(id__in=consultas_hoje.values('id'))
+
+    return render(request, 'consultas_medico.html', {'consultas_hoje': consultas_hoje, 'consultas_restantes': consultas_restantes, 'is_medico': is_medico(request.user)})
+
 
         
